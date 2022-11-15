a1621df79a7bac1c6f62c99ff40964e89c512a19
NilsonSIMAO <nilsonsimao@magicrecycle.com>
2022-11-15 10:09:19 +0100

    Update banco.php
    conectando o banco e mostrando os dados

--- includes/banco.php
+++ includes/banco.php
@@ -1,9 +1,17 @@
-<?php
-	$banco = new mysqli("localhost", "root", "", "bd_games");
+<pre><?php
+	$banco = new mysqli("localhost", "root","", "bd_games");
+	
 		if($banco->connect_errno){
 				echo "<p>Encontrei um erro $banco->errno --> 
 				$banco->connect_error
 				</p>";
 				die();
 		}
-		$banco->quert("select*fro generos");
+		$busca = $banco->query("select*from generos");
+		if(!$busca){
+			echo "<p> Falha na busca! $banco->error </p>";
+		} else {
+			while($reg = $busca->fetch_object()){
+			print_r($reg);
+			}
+		}
